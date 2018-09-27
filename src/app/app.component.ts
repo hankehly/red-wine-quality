f29653c5faf39c6c301cf6ecac8bac109c40b8d0
Hank Ehly <henry.ehly@gmail.com>
2018-09-27 12:57:52 +0900

    Add frontend template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'rw-app';
+
+    onClickEstimateQuality() {
+        console.log('clicked')
+    }
+
 }

6c686a25448b6a285c0d12aa581d956e85c499fe
Hank Ehly <henry.ehly@gmail.com>
2018-09-27 13:31:50 +0900

    Update frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import _ from 'lodash';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+    names = [
+        'Fixed Acidity',
+        'Volatile Acidity',
+        'Citric Acid',
+        'Residual Sugar',
+        'Chlorides',
+        'Free Sulfur Dioxide',
+        'Total Sulfur Dioxide',
+        'Density',
+        'pH',
+        'Sulphates',
+        'Alcohol',
+        'Estimated Quality'
+    ];
 
-    onClickEstimateQuality() {
-        console.log('clicked')
+    frame = [];
+
+    ngOnInit(): void {
+        this.frame.push(this.generateNewRow());
+    }
+
+    onClickAddRow(): void {
+        this.frame.push(this.generateNewRow())
+    }
+
+    onClickEstimateQuality(): void {
+        console.log('onClickEstimateQuality')
+    }
+
+    private generateNewRow(): number[] {
+        return _.times(this.names.length, _.constant(0));
     }
 
 }
